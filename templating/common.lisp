49a048679390a0dc795a1d7e941867e61d4a4009
Fernando Borretti <eudoxiahp@gmail.com>
2014-08-29 18:11:34 -0300

    Write benchmark results to file

--- templating/common.lisp
+++ templating/common.lisp
@@ -1,5 +1,3 @@
-(in-package :benchmarks)
-
 (declaim (optimize (speed 3) (debug 0)))
 
 (defparameter +list+ (loop for i from 0 to 10000 collecting i))
@@ -8,9 +6,13 @@
                          (setf (gethash item table) item))
                        table))
 
-(defmacro benchmark (name &rest body0)
-  `(let ((start (get-internal-real-time)))
-     ,@body
-     (format t "~A: ~6$~&" ,name
-             (/ (- (get-internal-real-time) start)
-                internal-time-units-per-second))))
+(defmacro benchmark (name &rest body)
+  `(with-open-file (stream "results.txt"
+                           :direction :output
+                           :if-exists :append
+                           :if-does-not-exist :create)
+     (let ((start (get-internal-real-time)))
+       ,@body
+       (format stream "~A: ~3$~&" ,name
+               (/ (- (get-internal-real-time) start)
+                  internal-time-units-per-second)))))
