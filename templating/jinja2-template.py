752686dd16398e4c7eeb4910d4b85044eb2e6a73
Fernando Borretti <eudoxiahp@gmail.com>
2014-08-29 18:11:43 -0300

    Python 3 fixes, write benchmarks to file

--- templating/jinja2-template.py
+++ templating/jinja2-template.py
@@ -10,14 +10,14 @@ List:
 </ol>
 Dictionary:
 <dl>
-  {% for key, value in dict.iteritems() %}
+  {% for key, value in dict.items() %}
     <dt>{{key}}</dt>
     <dd>{{value}}</dd>
   {% endfor %}
 </dl>
 '''
 
-LIST = [i for i in xrange(1,10000)]
+LIST = [i for i in range(1,10000)] # Python 3 range, not xrange
 DICT = {i:i*2 for i in LIST}
 
 if __name__ == '__main__':
@@ -25,5 +25,7 @@ if __name__ == '__main__':
     start = datetime.now()
     out = template.render(list=LIST, dict=DICT)
     end = datetime.now()
-    print "Jinja2:", (end-start).microseconds/1e6
+    duration = (end-start).microseconds/1e6
+    with open('results.txt', 'a') as file:
+        file.write('jinja2: %.3f\n' % duration)
 
